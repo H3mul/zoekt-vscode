dd66de1f6690061c24f75cda573f266ce1e05541
Phillip Dykman <phil.d324@gmail.com>
2025-09-22 13:35:05 -0700

    Add linter

---
+++ eslint.config.mjs
@@ -0,0 +1,45 @@
+import { defineConfig } from "eslint/config";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    extends: compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
+
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.node,
+        },
+
+        parser: tsParser,
+    },
+
+    rules: {
+        "@typescript-eslint/no-unused-vars": ["error", {
+            varsIgnorePattern: "^_",
+            argsIgnorePattern: "^_",
+        }],
+
+        "@typescript-eslint/no-explicit-any": ["off"],
+
+        "no-constant-condition": ["warn", {
+            checkLoops: false,
+        }],
+    },
+}]);
